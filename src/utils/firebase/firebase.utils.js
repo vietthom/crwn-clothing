177631f3ecbdf98d4d2b1be162476a77edd0c3f0
vietthom <thomle0418@gmail.com>
2023-04-01 23:15:29 -0600

    Firebase db set up

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth'
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBHrAu6QBH_pwnLKqiVLOuc561ztot343s",
   authDomain: "crwn-clothing-db-7ea51.firebaseapp.com",
@@ -25,3 +32,16 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+};
