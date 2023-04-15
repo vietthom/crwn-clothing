b853d92044d1d72f746693e36794d54cd2d4444b
vietthom <thomle0418@gmail.com>
2023-04-14 23:28:22 -0600

    Complete sign in page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -37,7 +37,16 @@ const SignInForm = () => {
       console.log(response);
       resetFormFields();
     } catch (error) {
-
+      switch(error.code) {
+        case 'auth/wrong-password':
+          alert('Incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('No user associated with this email');
+          break;
+        default:
+          console.log(error);
+      }
     }
   }
 
@@ -71,7 +80,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button buttonType="google" onClick={signInWithGoogle}>Google Sign In</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google Sign In</Button>
         </div>
       </form>
     </div>
